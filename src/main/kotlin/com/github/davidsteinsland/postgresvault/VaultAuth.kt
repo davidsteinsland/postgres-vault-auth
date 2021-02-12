71a25859c3e1f5122b551c7fd6fc6c9f5027267b
David Steinsland <david@steinsland.email>
2021-02-12 23:50:02 +0100

    vault mount path can be set from ui

--- src/main/kotlin/com/github/davidsteinsland/postgresvault/VaultAuth.kt
+++ src/main/kotlin/com/github/davidsteinsland/postgresvault/VaultAuth.kt
@@ -3,11 +3,20 @@ package com.github.davidsteinsland.postgresvault
 import com.fasterxml.jackson.core.JsonProcessingException
 import com.intellij.application.ApplicationThreadPool
 import com.intellij.credentialStore.Credentials
+import com.intellij.database.access.DatabaseCredentials
 import com.intellij.database.dataSource.DatabaseAuthProvider
 import com.intellij.database.dataSource.DatabaseConnectionInterceptor.ProtoConnection
 import com.intellij.database.dataSource.DatabaseCredentialsAuthProvider
 import com.intellij.database.dataSource.LocalDataSource
 import com.intellij.database.dataSource.url.JdbcUrlParserUtil
+import com.intellij.database.dataSource.url.template.MutableParametersHolder
+import com.intellij.database.dataSource.url.template.ParametersHolder
+import com.intellij.database.dataSource.url.ui.UrlPropertiesPanel.createLabelConstraints
+import com.intellij.database.dataSource.url.ui.UrlPropertiesPanel.createSimpleConstraints
+import com.intellij.openapi.project.Project
+import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.JBTextField
+import com.intellij.uiDesigner.core.GridLayoutManager
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,13 +24,9 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.future.future
 import java.io.IOException
 import java.util.concurrent.CompletionStage
+import javax.swing.JPanel
 
 class VaultAuth : DatabaseAuthProvider, CoroutineScope {
-    private companion object {
-        private fun determineCluster(host: String?) =
-            host?.contains("prod")?.takeIf { it }?.let { "prod-fss" } ?: "preprod-fss"
-    }
-
     private val vault = Vault()
 
     override val coroutineContext = SupervisorJob() + Dispatchers.ApplicationThreadPool + CoroutineName("VaultAuth")
@@ -32,27 +37,23 @@ class VaultAuth : DatabaseAuthProvider, CoroutineScope {
     override fun isApplicable(dataSource: LocalDataSource) =
         dataSource.dbms.isPostgres
 
+    override fun createWidget(
+        project: Project?,
+        credentials: DatabaseCredentials,
+        dataSource: LocalDataSource
+    ): DatabaseAuthProvider.AuthWidget {
+        return VaultWidget(dataSource)
+    }
+
     override fun intercept(connection: ProtoConnection, silent: Boolean): CompletionStage<ProtoConnection>? {
-        val url = connection.connectionPoint.url
-
-        val parser = JdbcUrlParserUtil.parsed(
-            connection.connectionPoint,
-            url
-        )!!
-
-        val database = parser.getParameter("database")
-        val host = parser.getParameter("host")
-        val cluster = determineCluster(host)
-
-        if (database == null || host == null) {
-            throw VaultAuthException("Failed to parse/extract host or database from the JDBC url")
-        }
+        val mountPath = connection.connectionPoint.additionalJdbcProperties["vault.path"]
+            ?: throw VaultAuthException("Vault Mount path is not set")
 
         return future {
             val json = try {
-                vault.readJson("postgresql/$cluster/creds/$database-readonly")
+                vault.readJson(mountPath)
             } catch (err: JsonProcessingException) {
-                throw VaultAuthException("Failed to fetch credentials from Vault (cluster: $cluster)", err)
+                throw VaultAuthException("Failed to fetch credentials from Vault", err)
             } catch (err: IOException) {
                 throw VaultAuthException("Failed to run vault command: ${err.message}", err)
             }
@@ -69,6 +70,64 @@ class VaultAuth : DatabaseAuthProvider, CoroutineScope {
                 Credentials(username, password),
                 true
             )
+        }
+    }
+
+    @Suppress("TooManyFunctions", "EmptyFunctionBlock", "MagicNumber")
+    private class VaultWidget(dataSource: LocalDataSource) : DatabaseAuthProvider.AuthWidget {
+        private val pathField = JBTextField()
+        private val panel = JPanel(GridLayoutManager(1, 6)).apply {
+            val pathLabel = JBLabel("Mount path")
+            add(pathLabel, createLabelConstraints(0, 0, pathLabel.preferredSize.getWidth()))
+            add(pathField, createSimpleConstraints(0, 1, 3))
+
+            // dataSource
+            val parser = JdbcUrlParserUtil.parsed(
+                dataSource.connectionConfig,
+                dataSource.url
+            )!!
+
+            val host = parser.getParameter("host") ?: return@apply
+            val db = parser.getParameter("database") ?: return@apply
+            determineMountPath(host, db)
+        }
+
+        override fun save(dataSource: LocalDataSource, copyCredentials: Boolean) {
+            dataSource.additionalJdbcProperties["vault.path"] = pathField.text
+        }
+
+        override fun reset(dataSource: LocalDataSource, copyCredentials: Boolean) {
+            pathField.text = (dataSource.additionalJdbcProperties["vault.path"] ?: "")
+        }
+
+        override fun updateFromUrl(holder: ParametersHolder) {
+            val host = holder.getParameter("host") ?: return
+            val database = holder.getParameter("database") ?: return
+            determineMountPath(host, database)
+        }
+
+        override fun updateUrl(holder: MutableParametersHolder) {}
+
+        private fun determineMountPath(host: String, database: String) {
+            val cluster = host.contains("prod").takeIf { it }?.let { "prod-fss" } ?: "preprod-fss"
+            pathField.text = "postgresql/$cluster/creds/$database-readonly"
+        }
+
+        override fun isPasswordChanged(): Boolean {
+            return false
+        }
+
+        override fun hidePassword() {
+        }
+
+        override fun reloadCredentials() {
+        }
+
+        override fun getComponent() = panel
+
+        override fun getPreferredFocusedComponent() = pathField
+
+        override fun forceSave() {
         }
     }
 
